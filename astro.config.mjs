166a0a7afe40a58d1d49f0aadd0cce336e07160b
Daniel Gonzalez <daniel@minvera-systems.io>
2025-11-20 19:26:00 -0800

    fix path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://minerva-systems.github.io/minerva-web',
-  // Base path for GitHub Pages deployment
-  // For local development, you can temporarily set this to '/' or access via /minerva-web/
-  base: '/minerva-web',
+  site: 'https://minerva-systems.io',
+  base: '/', // important for root deployment
 });
-
